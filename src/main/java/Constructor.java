39ab67e8884f425ecf6f575f9100899493a021df
ZgodaRafal <rafal-@hotmail.fr>
2017-01-07 19:58:43 +0100

    Commentaires

--- src/main/java/Constructor.java
+++ src/main/java/Constructor.java
@@ -8,20 +8,24 @@ public class Constructor {
 	public static void main(String[] args) {
 
 		Graph graph = new SingleGraph("Graph");
+		// CSS du Graph
 		graph.addAttribute("ui.stylesheet",
 				"url('file:///"+System.getProperty("user.dir")+"/files/stylesheet')");
 		graph.addAttribute("ui.quality");
 		graph.addAttribute("ui.antialias");
 
+		// Lecture du fichier DIMACS 
 		try {
 			@SuppressWarnings("resource")
 			BufferedReader reader = new BufferedReader(new FileReader(System.getProperty("user.dir")+"/files/aim-100-1_6-no-1.cnf"));
 			String line;
+			/* Boucle sur chaque ligne qui regarde le premier caractère (si le premier char est c, ignore la ligne */
 			while ((line = reader.readLine()) != null) {
 				if (line.length() >= 1) {
 					char first = line.charAt(0);
 					if (first == 'c') {
 						System.out.println("Commentaire");
+						/* Parcourt les caractères suivants pour recupérés les deux noeuds*/
 					} else if (first == 'e') {
 						int i = 1;
 						boolean token = true;
@@ -64,7 +68,7 @@ public class Constructor {
 							} else
 								token = false;
 						}
-
+						/* Fin du parcours de la ligne, création des deux sommets (si il n'existe pas) et de l'arrete */ 
 						System.out.println(node1 + " " + node2);
 						if (graph.getNode(node1) != null) {
 							System.out.println("Déja existant");
